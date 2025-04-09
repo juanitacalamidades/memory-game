65f7fdec654fcf665cea5283ddec36bc1bfc7713
yoanna <yoannarrd@gmail.com>
2025-04-09 17:29:02 +0200

    primera versión flex en el container: el hover en el grid no funciona

---
+++ functions.js
@@ -0,0 +1,28 @@
+
+//Crear grid dinámico: valora las dimensaiones del viewport para generar las celdas necesarias hasta cubrir ancho y alto.
+
+const tileSize = 25;
+const background = document.querySelector(".background-grid");
+
+
+function createGrid() {
+  background.innerHTML = ""; // Limpiar grid anterior
+
+  const cols = Math.ceil(window.innerWidth / tileSize);
+  const rows = Math.ceil(window.innerHeight / tileSize);
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      const cell = document.createElement("div");
+      cell.className = "grid-cell";
+      cell.style.left = `${x * tileSize}px`;
+      cell.style.top = `${y * tileSize}px`;
+      background.appendChild(cell);
+    }
+  }
+}
+
+createGrid();
+window.addEventListener("resize", createGrid);
+
+  
